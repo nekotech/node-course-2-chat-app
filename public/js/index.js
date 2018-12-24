e06e6ee7672026a216e89e6590688715d7055a52
Matthew Lee <leematthewrobert@yahoo.com.au>
2018-12-23 18:50:18 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,10 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('Connected to server');
-  socket.emit('createMessage', {
-    from: 'foo@bar.com',
-    text: 'Hello from browser'
-  });
 });
 
 socket.on('disconnect', function () {
